f936eddfe9745f825617c408ae1d5c5e7941f4e0
Pia Schilling <ps149@hdm-stuttgart.de>
2022-05-02 22:35:00 +0200

    feat: add auth logic
    closes #11, might need refactoring after wiring up with ui

--- project/arcudoc/de.hdm_stuttgart.api/src/main/java/de/hdm_stuttgart/data/service/ApiConstants.java
+++ project/arcudoc/de.hdm_stuttgart.api/src/main/java/de/hdm_stuttgart/data/service/ApiConstants.java
@@ -1,8 +1,53 @@
 package de.hdm_stuttgart.data.service;
 
+import de.hdm_stuttgart.data.api.ServiceProvider;
+import de.hdm_stuttgart.data.api.SupabaseAuthClient;
+
 public final class ApiConstants {
 
-    public final static String supabaseAuthUrl = "https://tkulvivaltoyyguivrsh.supabase.co/auth/v1/authorize?provider=";
+    private final static SupabaseAuthClient authClient = ServiceProvider.getSupabaseAuthClient(); //todo inject once guice is introduced
+
+    public final static String supabaseAuthUrlBase = "https://tkulvivaltoyyguivrsh.supabase.co/auth/v1/";
+    public final static String supabaseAuthUrl = "https://tkulvivaltoyyguivrsh.supabase.co/auth/v1/authorize?provider="; //todo refactor
 
     public final static String supabaseRestUrl = "https://tkulvivaltoyyguivrsh.supabase.co/rest/v1/";
+
+    public final static String apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdWx2aXZhbHRveXlndWl2cnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDg0OTk4NjgsImV4cCI6MTk2NDA3NTg2OH0.smUOZMVWkh6GoxYDuxM6Y8pkxySBUYz3ZxJTSihAY5o";
+
+    private static String accessToken; //todo move to profile class (api constants should not be responsible for that)
+    private static String refreshToken;
+    private static long expiresIn;
+    private static long tokenTimestamp;
+    private static boolean expired;
+
+
+    public static String getAccessToken() {
+        if(isExpired()){
+            authClient.getAccesstokenWithRefreshToken(apiKey,refreshToken);
+            return null; //todo implement action when token is expired (fetch new token)
+        }else{
+            return accessToken;
+        }
+    }
+
+    private static boolean isExpired() {
+        expired = tokenTimestamp + expiresIn < System.currentTimeMillis();
+        return expired;
+    }
+
+    public static void setAccessToken(String accessToken) {
+        ApiConstants.accessToken = accessToken;
+    }
+
+    public static void setRefreshToken(String refreshToken) {
+        ApiConstants.refreshToken = refreshToken;
+    }
+
+    public static void setExpiresIn(long expiresIn) {
+        ApiConstants.expiresIn = expiresIn;
+    }
+
+    public static void setTokenTimestamp(long tokenTimestamp) {
+        ApiConstants.tokenTimestamp = tokenTimestamp;
+    }
 }
--- project/arcudoc/de.hdm_stuttgart.login/src/main/java/de/hdm_stuttgart/login/integration/LoginController.java
+++ project/arcudoc/de.hdm_stuttgart.login/src/main/java/de/hdm_stuttgart/login/integration/LoginController.java
@@ -1,35 +1,25 @@
 package de.hdm_stuttgart.login.integration;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import de.hdm_stuttgart.login.model.AuthProvider;
 import de.hdm_stuttgart.data.service.ApiConstants;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.io.OutputStream;
 
 public class LoginController {
 
     private static final Logger log = LogManager.getLogger();
-    String authBaseUrl = ApiConstants.supabaseAuthUrl;
+    private final String authBaseUrl = ApiConstants.supabaseAuthUrl;
+
+    private final PortListener portListener = new PortListener();
 
     public String getAuthUrl(AuthProvider provider) {
-        switch (provider){
-            case GITLAB -> {return authBaseUrl + "gitlab";}
-            case GOOGLE -> {return authBaseUrl + "google";}
-            default -> log.error("No such provider defined: " + provider);
-        }
-        return " "; //todo change impl
+        return authBaseUrl + provider.toString().toLowerCase();
     }
 
-    public void startHttpListener(){
-
-    }
-
-    static class AuthHttpHandler {
-
+    public void startAuthFlow(){
+        portListener.startListener();
+        log.debug("Authentication flow started");
     }
 
 }
