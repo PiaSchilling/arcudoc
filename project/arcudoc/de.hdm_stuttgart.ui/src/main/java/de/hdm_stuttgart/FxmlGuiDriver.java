dc6d1c59590ead903d67952835a522fdc5a799a0
Pia Schilling <ps149@hdm-stuttgart.de>
2022-05-05 11:45:05 +0200

    fix: try to fix login scene
    Invalid URL or resource not found error when trying to start FxmlGuiDriver

--- project/arcudoc/de.hdm_stuttgart.ui/src/main/java/de/hdm_stuttgart/FxmlGuiDriver.java
+++ project/arcudoc/de.hdm_stuttgart.ui/src/main/java/de/hdm_stuttgart/FxmlGuiDriver.java
@@ -9,6 +9,13 @@ import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 /**
  * Driver class for a simple JavaFX demonstration.
  *
@@ -27,9 +34,18 @@ public class FxmlGuiDriver extends Application {
 
     public void start(Stage stage) throws Exception {
 
+       /* ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("/fxml/hello.fxml");
+
+        InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        BufferedReader reader = new BufferedReader(streamReader);
+
+        String result = reader.lines().collect(Collectors.joining());
+        System.out.println(result);*/
+
         log.info("Starting Hello JavaFX and Maven demonstration application");
 
-        final String fxmlFile = "/fxml/hello.fxml";
+        final String fxmlFile = "/fxml/login-scene.fxml";
         log.debug("Loading FXML for main view from: {}", fxmlFile);
         final FXMLLoader loader = new FXMLLoader();
         final Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
