b28ba6212e027fcb73d2782226129f0d28b7b143
Sara Tietze <st093@hdm-stuttgart.de>
2022-05-19 13:43:18 +0200

    feat: implement parts of docuController

--- project/arcudoc/de.hdm_stuttgart.ui/src/main/java/module-info.java
+++ project/arcudoc/de.hdm_stuttgart.ui/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module gui {
     requires javafx.fxml;
     requires org.apache.logging.log4j;
     requires de.hdm.stuttgart.login;
+    requires de.hdm.stuttgart.docu;
 
     opens de.hdm_stuttgart to javafx.fxml;
     exports de.hdm_stuttgart;
