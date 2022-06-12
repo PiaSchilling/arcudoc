155fd9f9def7b5e7dfa61ae49b0ba3f718c97529
Sara Tietze <st093@hdm-stuttgart.de>
2022-06-12 14:04:03 +0200

    feat: format date string

--- project/arcudoc/de.hdm_stuttgart.workspace/src/main/java/de/hdm_stuttgart/workspace/model/MemberProjectResponse.java
+++ project/arcudoc/de.hdm_stuttgart.workspace/src/main/java/de/hdm_stuttgart/workspace/model/MemberProjectResponse.java
@@ -1,9 +1,20 @@
 package de.hdm_stuttgart.workspace.model;
 
 import com.google.gson.annotations.SerializedName;
+import de.hdm_stuttgart.data.api.ProfileRepository;
 import de.hdm_stuttgart.workspace.service.IMemberProjectResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MemberProjectResponse implements IMemberProjectResponse {
+    private static final Logger log = LogManager.getLogger(MemberProjectResponse.class);
+
 
     @SerializedName("projects")
     private Project project;
@@ -44,7 +55,21 @@ public class MemberProjectResponse implements IMemberProjectResponse {
 
     @Override
     public String getLastUpdated() {
-        return project.getLastUpdated();
+        String formattedDate = new String();
+        System.out.println("Date String: " + project.getLastUpdated());
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        try{
+            Date date = new Date(sdf.parse(project.getLastUpdated()).getTime());
+            System.out.println(date);
+            DateFormat dateFormat = new SimpleDateFormat("'last modified:' HH:mm 'Uhr' - dd.MM.yyyy");
+            formattedDate = dateFormat.format(date);
+            System.out.println(formattedDate);
+
+        } catch (ParseException e) {
+            log.error("Failed parsing project creation date " + e.getMessage());
+        }
+
+        return formattedDate;
     }
 
     @Override
