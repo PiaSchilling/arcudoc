50c7d594799b7ac6bd407384800fe0c8c3ac640f
paul <ps152@hdm-stuttgart.de>
2022-06-13 18:22:04 +0200

    feat: Implement Repo

--- project/arcudoc/de.hdm_stuttgart.docu/src/main/java/de/hdm_stuttgart/docu/data/DocuRepo.java
+++ project/arcudoc/de.hdm_stuttgart.docu/src/main/java/de/hdm_stuttgart/docu/data/DocuRepo.java
@@ -2,12 +2,18 @@ package de.hdm_stuttgart.docu.data;
 
 import de.hdm_stuttgart.data.service.ApiConstants;
 import de.hdm_stuttgart.docu.model.*;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.ObservableList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 import java.util.List;
+
 
 
 public class DocuRepo {
@@ -15,6 +21,9 @@ public class DocuRepo {
     private static final Logger log = LogManager.getLogger(DocuRepo.class);
     private final SupabaseRestClient supabaseRestClient = ServiceProvider.getSupabaseRestClient();
     private final int projectId = 1;
+    private List template;
+
+    private final ListProperty<TemplateResponse> templateResponseProperty = new SimpleListProperty<>();
 
 
     /**
@@ -31,7 +40,31 @@ public class DocuRepo {
 
         );
 
+        call.enqueue(new Callback<List<TemplateResponse>>() {
+            @Override
+            public void onResponse(Call<List<TemplateResponse>> call, Response<List<TemplateResponse>> response) {
+                if (response.isSuccessful()) {
+                    log.debug(response.code() + " Template has been loaded");
+                   template = response.body();
+                    templateResponseProperty.setValue((ObservableList<TemplateResponse>) template);
 
+                } else {
+                    log.error(response.code() + response.message() + " Template was not loaded ");
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<TemplateResponse>> call, Throwable throwable) {
+                log.error(throwable.getMessage() + " Template was not loaded - fetch failed");
+            }
+        });
+
+
+    }
+
+    public ListProperty<TemplateResponse> getTemplateResponseProperty() {
+        return templateResponseProperty;
     }
 
 }
