3087dde976eb0fe223c640f05803a00bbb4bb634
paul <ps152@hdm-stuttgart.de>
2022-06-13 18:20:37 +0200

    feat: Implement Docu Controller to communicate with the UI

--- project/arcudoc/de.hdm_stuttgart.docu/src/main/java/de/hdm_stuttgart/docu/integration/DocuController.java
+++ project/arcudoc/de.hdm_stuttgart.docu/src/main/java/de/hdm_stuttgart/docu/integration/DocuController.java
@@ -1,9 +1,38 @@
 package de.hdm_stuttgart.docu.integration;
 
+import com.google.inject.Inject;
+import de.hdm_stuttgart.docu.data.DocuRepo;
+import de.hdm_stuttgart.docu.model.TemplateResponse;
+import de.hdm_stuttgart.docu.service.ITemplateResponse;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+
 public class DocuController {
 
+    private final DocuRepo docuRepo;
+    private final ListProperty<ITemplateResponse> templateResponseListProperty;
 
-   public void printTest(){
-       System.out.println("Method in controller triggered");
-   }
+    @Inject
+    public DocuController(DocuRepo docuRepo) {
+        this.docuRepo = docuRepo;
+
+        ListProperty<TemplateResponse> templateResponseRepo = docuRepo.getTemplateResponseProperty();
+        templateResponseListProperty = new SimpleListProperty<>();
+        templateResponseRepo.addListener((observable, oldValue, newValue) -> updateTemplateResponseProperty());
+
+    }
+
+    private void updateTemplateResponseProperty(){
+        ObservableList<ITemplateResponse> observableList = FXCollections.observableArrayList();
+        observableList.addAll(docuRepo.getTemplateResponseProperty());
+        templateResponseListProperty.setValue(observableList);
+    }
+
+    public ListProperty<ITemplateResponse> getTemplateResponseListProperty() {
+        docuRepo.getTemplate();
+        return templateResponseListProperty;
+    }
 }
