4fa350a42f55e4471a31aa3f30426fda3f2a8eb4
paul <ps152@hdm-stuttgart.de>
2022-06-17 16:50:58 +0200

    feat: add setter for Template

--- project/arcudoc/de.hdm_stuttgart.docu/src/main/java/de/hdm_stuttgart/docu/service/ITemplateResponse.java
+++ project/arcudoc/de.hdm_stuttgart.docu/src/main/java/de/hdm_stuttgart/docu/service/ITemplateResponse.java
@@ -2,12 +2,12 @@ package de.hdm_stuttgart.docu.service;
 
 import com.google.gson.JsonArray;
 
-import java.util.List;
-
 public interface ITemplateResponse {
 
     JsonArray getTemplate();
 
     String getTitle();
+
+    void setTemplate(JsonArray template);
 
 }
