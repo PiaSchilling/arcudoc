66a1ce4310db133667173c3cb17c93e5ca56d036
Pia Schilling <ps149@hdm-stuttgart.de>
2022-06-19 10:55:23 +0200

    fix: clean up module info in ui module

--- project/arcudoc/de.hdm_stuttgart.ui/src/main/java/module-info.java
+++ project/arcudoc/de.hdm_stuttgart.ui/src/main/java/module-info.java
@@ -1,17 +1,19 @@
 module gui {
+
+    //module dependencies
+    requires de.hdm.stuttgart.login;
+    requires workspace;
+    requires de.hdm.stuttgart.editor;
+    requires de.hdm.stuttgart.docu;
+    requires de.hdm.stuttgart.api;
+
+    //external dependencies
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
     requires org.apache.logging.log4j;
     requires com.google.guice;
-
-    requires de.hdm.stuttgart.login;
-    requires workspace;
-    requires de.hdm.stuttgart.editor;
-    uses de.hdm_stuttgart.login.service.ILogin;
-    requires de.hdm.stuttgart.docu;
     requires java.prefs;
-    requires de.hdm.stuttgart.api;
     requires com.google.gson;
 
     uses com.google.inject.AbstractModule;
@@ -20,13 +22,10 @@ module gui {
     opens de.hdm_stuttgart.login to javafx.fxml, javafx.graphics, javafx.controls;
     opens de.hdm_stuttgart.workspace to javafx.fxml, javafx.graphics, javafx.controls;
     opens de.hdm_stuttgart.project to javafx.fxml, javafx.graphics, javafx.controls;
-
-
-    exports de.hdm_stuttgart.login to javafx.fxml, javafx.graphics, javafx.controls;
-    exports de.hdm_stuttgart;
+    opens de.hdm_stuttgart.navigation to javafx.controls, javafx.fxml, javafx.graphics;
+    exports de.hdm_stuttgart to com.google.guice;
+    exports de.hdm_stuttgart.navigation;
 
     opens images;
     opens styles;
-    exports de.hdm_stuttgart.navigation;
-    opens de.hdm_stuttgart.navigation to javafx.controls, javafx.fxml, javafx.graphics;
 }
